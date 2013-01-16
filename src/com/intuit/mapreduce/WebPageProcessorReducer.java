566abd97164666815b404a61382db17c5dc6cdc0
RG <roopnath_grandhi@intuit.com>
2013-01-16 15:32:22 -0800

    refactored and added comments

--- src/com/intuit/mapreduce/WebPageProcessorReducer.java
+++ src/com/intuit/mapreduce/WebPageProcessorReducer.java
@@ -12,7 +12,11 @@ import org.apache.hadoop.mapred.Reporter;
 import org.commoncrawl.hadoop.mapred.ArcRecord;
 
 
-
+/**
+ * Reducer class for processing the webpage as "Arcrecord"
+ * @author rgrandhi
+ *
+ */
 
 
 public class WebPageProcessorReducer  
@@ -20,14 +24,11 @@ extends    MapReduceBase
 implements Reducer<Text, Text, Text, Text>
 
 {
-
-	
-	
 	public void reduce(Text key, Iterator<Text> iter, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-        long sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0;
+        long sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0;
         String text = "";
 	
-        // Summates all word counts for this word.
+        // sum all the output variable counts across all the values for the current URL.
         while (iter.hasNext()) {
 		
             String s[] = (iter.next().toString()).split("///");
@@ -37,13 +38,14 @@ implements Reducer<Text, Text, Text, Text>
             sum3 += Integer.parseInt(s[2]);
             sum4 += Integer.parseInt(s[3]);
             sum5 += Integer.parseInt(s[4]);
-            if (s.length > 5) {
-                text = text + s[5];
+            sum6 += Integer.parseInt(s[5]);
+            if (s.length > 6) {
+                text = text + s[6];
             }
         }
         output.collect(key,
                 new Text(
-                sum1 + "///" + sum2 + "///" + sum3 + "///" + sum4 + "///" + sum5));
+                sum1 + "///" + sum2 + "///" + sum3 + "///" + sum4 + "///" + sum5 + "///" + sum6));
     }
 
 
