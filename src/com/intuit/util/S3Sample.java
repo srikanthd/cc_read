63f7213a004f2f7122881241582eff3f663b3828
RG <roopnath_grandhi@intuit.com>
2013-01-16 15:33:26 -0800

    added comments

--- src/com/intuit/util/S3Sample.java
+++ src/com/intuit/util/S3Sample.java
@@ -2,7 +2,9 @@ package com.intuit.util;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,8 +12,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import com.amazonaws.AmazonClientException;
@@ -28,61 +33,169 @@ import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 /**
- * This sample demonstrates how to make basic requests to Amazon S3 using
- * the AWS SDK for Java.
- * <p>
- * <b>Prerequisites:</b> You must have a valid Amazon Web Services developer
- * account, and be signed up to use Amazon S3. For more information on
- * Amazon S3, see http://aws.amazon.com/s3.
- * <p>
- * <b>Important:</b> Be sure to fill in your AWS access credentials in the
- *                   AwsCredentials.properties file before you try to run this
- *                   sample.
- * http://aws.amazon.com/security-credentials
+ * Used to connect to S3 and generate the complete list of arc.gz files for 2010 and
+ * 2012 commoncrawl data sets.
+ * 
+ * 
+ * @author rgrandhi
+ *
  */
+
+
 public class S3Sample {
+	
+	//Read the valid segment ids for 2012 data.
+	public static Set<String> readValidSegments()
+	{
+		String file = "C:\\Users\\rgrandhi\\workspace\\crawlproj\\src\\com\\intuit\\util\\valid_segments.txt";	
+		
+		Set<String> validSegments = new HashSet<String>();
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = "";
+			
+			while((line=reader.readLine())!=null)
+			{
+				validSegments.add(line);
+				
+				
+			}
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return validSegments;
+		
+		
+		
+	}
 
-    public static void main(String[] args) throws IOException {
-        /*
-         * Important: Be sure to fill in your AWS access credentials in the
-         *            AwsCredentials.properties file before you try to run this
-         *            sample.
-         * http://aws.amazon.com/security-credentials
-         */
-        AmazonS3 s3 = new AmazonS3Client(new PropertiesCredentials(
-                S3Sample.class.getResourceAsStream("AwsCredentials.properties")));
+	
+	//Generate 2010 list of arc.gz files.
+	public static void generate2010ArcFiles()
+	{
 
-        String bucketName = "my-first-s3-bucket-" + UUID.randomUUID();
-        String key = "MyObjectKey";
+		String prefix = "common-crawl/crawl-002/2010/";
+    	String fileName =  "C:\\Users\\rgrandhi\\workspace\\crawlproj\\src\\com\\intuit\\util\\" + "2010_arcfiles.txt";
+    	
+    	
+		List<S3ObjectSummary> keyList = extractPrefixFiles(prefix);
+    	PrintWriter writer;
+		try {
+			writer = new PrintWriter(new FileWriter(fileName));
+	    	for (Iterator iterator = keyList.iterator(); iterator.hasNext();) {
+				S3ObjectSummary s3ObjectSummary = (S3ObjectSummary) iterator
+						.next();
+				String arcFileName = s3ObjectSummary.getKey();
+							
+				if(arcFileName.indexOf("arc.gz")>=0)
+				{
+					writer.println(arcFileName);
+				}
+				
+		} 
+	    	
+			writer.close();
 
-        System.out.println("===========================================");
-        System.out.println("Getting Started with Amazon S3");
-        System.out.println("===========================================\n");
+		}	catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
 
+		
+		}
+    	
+    
+	
+	//Generate 2012 list of arc.gz files using valid segment ids.
+	public static void generate2012ArcFiles()
+	{
+
+		String prefix = "common-crawl/parse-output/segment/";
+    	String fileName =  "C:\\Users\\rgrandhi\\workspace\\crawlproj\\src\\com\\intuit\\util\\" + "2012_arcfiles.txt";
+    	Set<String> validSegments = readValidSegments();
+    	List<S3ObjectSummary> keyList = new ArrayList<S3ObjectSummary>();
+    	
+    	//Iterate through each of the paths of valid segments and generate the list of arc.gz files within each path.
+    	for (Iterator iterator = validSegments.iterator(); iterator.hasNext();) {
+			String segID = (String) iterator.next();
+			String curPrefix = prefix + segID + "/";
+			System.out.println("Processing...." + curPrefix);
+					
+			List<S3ObjectSummary> curKeyList = extractPrefixFiles(curPrefix);
+			keyList.addAll(curKeyList);
+    	}
+    	
+    	//print the list of arc.gz files into the output file.
+    	PrintWriter writer;
+		try {
+			writer = new PrintWriter(new FileWriter(fileName));
+	    	for (Iterator iter = keyList.iterator(); iter.hasNext();) {
+				S3ObjectSummary s3ObjectSummary = (S3ObjectSummary) iter
+						.next();
+				String arcFileName = s3ObjectSummary.getKey();
+							
+				if(arcFileName.indexOf("arc.gz")>=0)
+				{
+					writer.println(arcFileName);
+				}
+			
+				
+		} 
+	    	
+			writer.close();
+
+		}	catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+
+		
+		}
+    	
+		
+	
+	//Given a prefix path, generate the list of all files in the S3 which has that path as prefix.
+	
+	public static List<S3ObjectSummary> extractPrefixFiles(String prefix)
+	{
+		
+        AmazonS3 s3=null;
+        List<S3ObjectSummary> keyList = null;
+		try {
+			s3 = new AmazonS3Client(new PropertiesCredentials(
+			        S3Sample.class.getResourceAsStream("AwsCredentials.properties")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+       
         try {
            
-
-            /*
-             * List objects in your bucket by prefix - There are many options for
-             * listing the objects in your bucket.  Keep in mind that buckets with
-             * many objects might truncate their results when listing their objects,
-             * so be sure to check if the returned object listing is truncated, and
-             * use the AmazonS3.listNextBatchOfObjects(...) operation to retrieve
-             * additional results.
-             */
-        	
-        	bucketName = "aws-publicdatasets";
-        	String prefix = "common-crawl/crawl-002/2010/";
-        	String fileName =  "C:\\Users\\rgrandhi\\workspace\\crawlproj\\src\\com\\intuit\\util\\" + "2010_arcfiles.txt";	
-        			
+        	String bucketName = "aws-publicdatasets";
+        				
         	ObjectListing current = s3.listObjects(bucketName,prefix);
-        	List<S3ObjectSummary> keyList = current.getObjectSummaries();
+        	keyList = current.getObjectSummaries();
         	
+        	System.out.println(keyList.size());
         
         	
         	ObjectListing next = s3.listNextBatchOfObjects(current);
         	keyList.addAll(next.getObjectSummaries());
 
+        	//Iterate through the set of batch objects until no new files exist with the given prefix.
         	while (next.isTruncated()) {
         	   current=s3.listNextBatchOfObjects(next);
         	   keyList.addAll(current.getObjectSummaries());
@@ -91,33 +204,6 @@ public class S3Sample {
         	}
         	keyList.addAll(next.getObjectSummaries());
         	
-        	
-        	PrintWriter writer = new PrintWriter(new FileWriter(fileName));
-        	
-        	for (Iterator iterator = keyList.iterator(); iterator.hasNext();) {
-				S3ObjectSummary s3ObjectSummary = (S3ObjectSummary) iterator
-						.next();
-				String arcFileName = s3ObjectSummary.getKey();
-				
-				if(arcFileName.indexOf("arc.gz")>=0)
-				{
-					writer.println(arcFileName);
-				}
-				
-			}
-        	
-        	writer.close();
-        	
-           /* System.out.println("Listing objects");
-            ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
-                    .withBucketName("aws-publicdatasets")
-                    .withPrefix("common-crawl/crawl-002/2010/"));
-            for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
-                System.out.println(" - " + objectSummary.getKey() + "  " +
-                                   "(size = " + objectSummary.getSize() + ")");
-            }
-            System.out.println();
-*/
            
         } catch (AmazonServiceException ase) {
             System.out.println("Caught an AmazonServiceException, which means your request made it "
@@ -133,48 +219,18 @@ public class S3Sample {
                     + "such as not being able to access the network.");
             System.out.println("Error Message: " + ace.getMessage());
         }
-    }
 
-    /**
-     * Creates a temporary file with text data to demonstrate uploading a file
-     * to Amazon S3
-     *
-     * @return A newly created temporary file with text data.
-     *
-     * @throws IOException
-     */
-    private static File createSampleFile() throws IOException {
-        File file = File.createTempFile("aws-java-sdk-", ".txt");
-        file.deleteOnExit();
-
-        Writer writer = new OutputStreamWriter(new FileOutputStream(file));
-        writer.write("abcdefghijklmnopqrstuvwxyz\n");
-        writer.write("01234567890112345678901234\n");
-        writer.write("!@#$%^&*()-=[]{};':',.<>/?\n");
-        writer.write("01234567890112345678901234\n");
-        writer.write("abcdefghijklmnopqrstuvwxyz\n");
-        writer.close();
-
-        return file;
-    }
-
-    /**
-     * Displays the contents of the specified input stream as text.
-     *
-     * @param input
-     *            The input stream to display as text.
-     *
-     * @throws IOException
-     */
-    private static void displayTextInputStream(InputStream input) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        while (true) {
-            String line = reader.readLine();
-            if (line == null) break;
-
-            System.out.println("    " + line);
-        }
-        System.out.println();
+		
+		
+		return keyList;
+		
+		
+	}
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	//generate2010ArcFiles();
+    	generate2012ArcFiles();
     }
 
 }
