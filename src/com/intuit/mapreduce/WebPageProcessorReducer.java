e168fc4c0169632c024d18b050ac12ca7c106c37
RG <roopnath_grandhi@intuit.com>
2013-01-27 22:42:04 -0800

    tracking of histogram of anchor text words and latest changes

--- src/com/intuit/mapreduce/WebPageProcessorReducer.java
+++ src/com/intuit/mapreduce/WebPageProcessorReducer.java
@@ -14,7 +14,7 @@ import org.commoncrawl.hadoop.mapred.ArcRecord;
 
 /**
  * Reducer class for processing the webpage as "Arcrecord"
- * @author rgrandhi
+ * 
  *
  */
 
@@ -25,13 +25,21 @@ implements Reducer<Text, Text, Text, Text>
 
 {
 	public void reduce(Text key, Iterator<Text> iter, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-        long sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0;
-        String text = "";
-	
+        long sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0 , sum7=0, sum8=0 , anchorTextMatchesLink =0;
+        String pageAnchorText = "";
+        String inBoundAnchorText = "";
+        
+        long anchorTextWordHistogram[] = {0,0,0,0,0,0};
+        String anchorTextWordHistogramStr = "";
+        boolean histogramOutput =false;
+        
         // sum all the output variable counts across all the values for the current URL.
         while (iter.hasNext()) {
 		
-            String s[] = (iter.next().toString()).split("///");
+        	String curStr = iter.next().toString();
+        	
+        	//System.out.println("current value str::" + curStr);
+            String s[] = curStr.split("///",-1);
 
             sum1 += Integer.parseInt(s[0]);
             sum2 += Integer.parseInt(s[1]);
@@ -39,13 +47,55 @@ implements Reducer<Text, Text, Text, Text>
             sum4 += Integer.parseInt(s[3]);
             sum5 += Integer.parseInt(s[4]);
             sum6 += Integer.parseInt(s[5]);
-            if (s.length > 6) {
-                text = text + s[6];
+            sum7 += Integer.parseInt(s[6]);
+            sum8 += Integer.parseInt(s[7]);
+            
+            //if anchor text is output.
+            if (s.length > 8 && s[8].length()>0) {
+            	if(inBoundAnchorText.length()>0) inBoundAnchorText += "$#";
+            	inBoundAnchorText = inBoundAnchorText + s[8];
             }
-        }
+            
+            
+            //if anchor text word histogram is output. compute the sum of histogram values.
+            if (s.length > 9 && s[9].length()>0) {
+            	//System.out.println("anchor text histogram::" + s[9]);
+            	String histogramFields[] = s[9].split("#",-1);
+            	for(int m=0;m<anchorTextWordHistogram.length;++m)
+            	{
+            		anchorTextWordHistogram[m] += Long.parseLong(histogramFields[m]);
+            	}
+            	histogramOutput = true;
+            }
+            	
+            
+            //anchor text matches the link.
+            if (s.length > 10 && s[10].length()>0) 
+            {
+            	anchorTextMatchesLink += Integer.parseInt(s[10]);
+		
+            }
+            
+            
+            
+            }
+            
+        
+	    //if histogram is output, compute the updated string representation of the histogram.
+		if(histogramOutput)
+		{
+			for(int m=0 ; m<anchorTextWordHistogram.length ; ++m)
+			{
+				if(anchorTextWordHistogramStr.length()>0) anchorTextWordHistogramStr += "#";
+				anchorTextWordHistogramStr += String.valueOf(anchorTextWordHistogram[m]);
+			}
+		  	
+		}
+		
         output.collect(key,
                 new Text(
-                sum1 + "///" + sum2 + "///" + sum3 + "///" + sum4 + "///" + sum5 + "///" + sum6));
+                sum1 + "///" + sum2 + "///" + sum3 + "///" + sum4 + "///" + sum5 + "///" + sum6 + "///" + sum7 + "///" + sum8 + "///" + inBoundAnchorText + "///" + anchorTextWordHistogramStr + "///" + anchorTextMatchesLink ));
+                
     }
 
 
